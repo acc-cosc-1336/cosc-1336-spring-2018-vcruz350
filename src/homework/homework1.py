f3945a4a322ee638c27bf27464bfca7493b815e0
vcruz350 <35586950+vcruz350@users.noreply.github.com>
2018-01-24 21:16:53 -0600

    Update homework1.py

--- src/homework/homework1.py
+++ src/homework/homework1.py
@@ -1,26 +1,34 @@
+#calculate hours, minutes and seconds
+
 def get_hours_since_midnight(seconds):
+    total_hours = seconds // 3600
     '''
     Type the code to calculate total hours given n(number) of seconds
     For example, given 3800 seconds the total hours is 1
     '''
-    return
+    return total_hours
 
 '''
 IF YOU ARE OK WITH A GRADE OF 70 FOR THIS ASSIGNMENT STOP HERE.
 '''
 
 def get_minutes(seconds):
+    remaining_seconds = seconds % 3600
+    total_minutes = remaining_seconds // 60
     '''
     Type the code to calculate total minutes less whole hour given n(number) of seconds
     For example, given 3800 seconds the total minutes is 3
     '''
 
-    return
+    return total_minutes
 
 def get_seconds(seconds):
+    remaining_seconds = seconds % 3600
+    total_minutes = remaining_seconds // 60
+    total_seconds = total_minutes % 60
     '''
     Type the code to calculate total minutes less whole hour given n(number) of seconds
     For example, given 3800 seconds the total minutes is 20
     '''
 
-    return
+    return total_seconds
