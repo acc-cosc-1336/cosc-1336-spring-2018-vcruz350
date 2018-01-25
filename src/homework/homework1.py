3a9a8dc58a5209eff35823e492b68e26fc220d6c
vcruz350 <35586950+vcruz350@users.noreply.github.com>
2018-01-24 22:41:54 -0600

    Update homework1.py
    Removed notes and added input and print functions.

--- src/homework/homework1.py
+++ src/homework/homework1.py
@@ -1,34 +1,28 @@
-#calculate hours, minutes and seconds
 
+#determine number of hours
 def get_hours_since_midnight(seconds):
     total_hours = seconds // 3600
-    '''
-    Type the code to calculate total hours given n(number) of seconds
-    For example, given 3800 seconds the total hours is 1
-    '''
     return total_hours
 
-'''
-IF YOU ARE OK WITH A GRADE OF 70 FOR THIS ASSIGNMENT STOP HERE.
-'''
-
+#determine number of minutes
 def get_minutes(seconds):
     remaining_seconds = seconds % 3600
     total_minutes = remaining_seconds // 60
-    '''
-    Type the code to calculate total minutes less whole hour given n(number) of seconds
-    For example, given 3800 seconds the total minutes is 3
-    '''
-
     return total_minutes
 
+#determine number of seconds
 def get_seconds(seconds):
     remaining_seconds = seconds % 3600
     total_minutes = remaining_seconds // 60
-    total_seconds = total_minutes % 60
-    '''
-    Type the code to calculate total minutes less whole hour given n(number) of seconds
-    For example, given 3800 seconds the total minutes is 20
-    '''
-
+    total_seconds = remaining_seconds - total_minutes * 60 
     return total_seconds
+
+seconds = int(input(' Enter number of seconds after midnight: '))
+hours = (get_hours_since_midnight(seconds))
+minutes = (get_minutes(seconds))
+second = (get_seconds(seconds))
+
+print(hours, ":", minutes, ":", second)
+
+        
+
