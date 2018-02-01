9a6c7176030516b8ace7b19d2e5fb3fdad37308e
vcruz350 <35586950+vcruz350@users.noreply.github.com>
2018-01-31 22:06:41 -0600

    Update homework1.py

--- src/homework/homework1.py
+++ src/homework/homework1.py
@@ -2,27 +2,17 @@
 #determine number of hours
 def get_hours_since_midnight(seconds):
     total_hours = seconds // 3600
-    return total_hours
+    return ('Hours:' , total_hours)
 
 #determine number of minutes
 def get_minutes(seconds):
     remaining_seconds = seconds % 3600
     total_minutes = remaining_seconds // 60
-    return total_minutes
+    return ('Minutes:' , total_minutes)
 
 #determine number of seconds
 def get_seconds(seconds):
     remaining_seconds = seconds % 3600
     total_minutes = remaining_seconds // 60
     total_seconds = remaining_seconds - total_minutes * 60 
-    return total_seconds
-
-seconds = int(input(' Enter number of seconds after midnight: '))
-hours = (get_hours_since_midnight(seconds))
-minutes = (get_minutes(seconds))
-second = (get_seconds(seconds))
-
-print(hours, ":", minutes, ":", second)
-
-        
-
+    return ('Seconds:' , total_seconds)
