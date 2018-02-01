fa8aad3b589225a4733b56627e5be8a325ea9f39
vcruz350 <35586950+vcruz350@users.noreply.github.com>
2018-02-01 11:50:51 -0600

    Update homework2.py

--- src/homework/homework2.py
+++ src/homework/homework2.py
@@ -27,7 +27,7 @@ def get_time(hour, minutes, seconds, time_type, meridiem='AM'):
     if time_type == 24 and (hour < 0 or hour > 23):
         return 'Invalid hours(range 0-23)'
     if time_type != 12 and time_type != 24:
-        return 'Invalid time_type(12 or 24hr only)'
+        return 'Invalid time_type(12 or 24 only)'
     if minutes < 0 or minutes > 59:
         return 'Invalid minutes(range 0-59)'
     if seconds < 0 or seconds > 59:
