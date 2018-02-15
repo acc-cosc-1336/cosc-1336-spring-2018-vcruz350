afb338f063ce16e5d0d3e44c163ed3beda525355
vcruz350 <35586950+vcruz350@users.noreply.github.com>
2018-02-14 21:38:04 -0600

    Add files via upload

--- run_app.py
+++ run_app.py
@@ -1,4 +1,4 @@
-import src.homework.main.main_homework2
+import src.homework.main.main_homework4
 
 #running this file will run the code in main_homework2.py file without the need to modify
 #code in that file
--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from tests.homework import test_homework2
+from tests.homework import test_homework4
 
-suite = unittest.TestLoader().loadTestsFromModule(test_homework2)
+suite = unittest.TestLoader().loadTestsFromModule(test_homework4)
 unittest.TextTestRunner(verbosity=2).run(suite)
