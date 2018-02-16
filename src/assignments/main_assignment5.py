26e5b8bd2fe437127ecf89679ea25301bda3c58b
vcruz350 <35586950+vcruz350@users.noreply.github.com>
2018-02-15 21:32:35 -0600

    Add files via upload

---
+++ src/assignments/main_assignment5.py
@@ -0,0 +1,22 @@
+#write import statement for recursive_decimal_binary
+from src.assignments.assignment5 import recursive_decimal_binary
+'''
+Write a for loop from 1 to 255 and call the recursive_decimal_binary function with loop index/target variable
+as an argument and print the decimal and binary value next to each other as follows:
+1 00000001
+2 00000010
+3 00000011
+4 00000100
+5 00000101
+6 00000110
+7 00000111
+etc
+'''
+
+def main():
+    for num in range (1,256):
+        result = recursive_decimal_binary (num,7)
+
+        print(num,result)
+
+main()
